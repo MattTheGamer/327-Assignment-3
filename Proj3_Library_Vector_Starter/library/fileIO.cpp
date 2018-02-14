ab845d5ad24aedcb2ee21bf0601265023bb4ac2c
matt <matt@matt-VirtualBox>
2018-02-14 11:46:13 -0500

    Wrote LoadBooks and reloadAllData

--- Proj3_Library_Vector_Starter/library/fileIO.cpp
+++ Proj3_Library_Vector_Starter/library/fileIO.cpp
@@ -1,13 +1,66 @@
 #include "../includes_usr/fileIO.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
 using namespace std;
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries in books
  * 			SUCCESS if all data is loaded
  * */
-int loadBooks(std::vector<book> &books, const char* filename)
-{
-	return SUCCESS;
+int loadBooks(std::vector<book> &books, const char* filename) {
+	ifstream inFile;
+	string line;
+	string token;
+	stringstream ss;
+	const char CHAR_TO_SEARCH_FOR = ',';
+	book book;
+
+	inFile.open(filename);
+	if (!inFile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	} else {
+		if (inFile.peek() == std::ifstream::traits_type::eof()) {
+			return NO_BOOKS_IN_LIBRARY;
+		} else {
+
+			while (!inFile.eof()) {
+				getline(inFile, line);
+				ss.str(line);
+
+				getline(ss, token, CHAR_TO_SEARCH_FOR);
+				book.book_id = atoi(token.c_str());
+
+				getline(ss, book.title, CHAR_TO_SEARCH_FOR);
+
+				getline(ss, book.author, CHAR_TO_SEARCH_FOR);
+
+				getline(ss, token, CHAR_TO_SEARCH_FOR);
+				int temp = atoi(token.c_str());
+
+				switch (temp) {
+
+				case 0:
+					book.state = UNKNOWN;
+					break;
+				case 1:
+					book.state = IN;
+					break;
+				case 2:
+					book.state = OUT;
+					break;
+				}
+				getline(ss, token, CHAR_TO_SEARCH_FOR);
+				book.loaned_to_patron_id = atoi(token.c_str());
+
+				books.push_back(book);
+
+				ss.clear();
+			}
+			return SUCCESS;
+		}
+	}
+
 }
 
 /* serializes books to the file filename
@@ -15,8 +68,7 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * 			NO_BOOKS_IN_LIBRARY if there are 0 entries books (do not create file)
  * 			SUCCESS if all data is saved
  * */
-int saveBooks(std::vector<book> &books, const char* filename)
-{
+int saveBooks(std::vector<book> &books, const char* filename) {
 	return SUCCESS;
 }
 
@@ -25,8 +77,7 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * 			NO_PATRONS_IN_LIBRARY if there are 0 entries in patrons
  * 			SUCCESS if all data is loaded
  * */
-int loadPatrons(std::vector<patron> &patrons, const char* filename)
-{
+int loadPatrons(std::vector<patron> &patrons, const char* filename) {
 	return SUCCESS;
 }
 
@@ -35,7 +86,6 @@ int loadPatrons(std::vector<patron> &patrons, const char* filename)
  * 			NO_PATRONS_IN_LIBRARY if there are 0 entries in patrons  (do not create file)
  * 			SUCCESS if all data is saved
  * */
-int savePatrons(std::vector<patron> &patrons, const char* filename)
-{
+int savePatrons(std::vector<patron> &patrons, const char* filename) {
 	return SUCCESS;
 }
