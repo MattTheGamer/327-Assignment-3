ff2a21a9b71f720ee336842fb4fa5e27b8a4840f
matt <matt@matt-VirtualBox>
2018-02-15 16:32:25 -0500

    Finished the FILEIO.cpp

--- Proj3_Library_Vector_Starter/library/fileIO.cpp
+++ Proj3_Library_Vector_Starter/library/fileIO.cpp
@@ -17,6 +17,7 @@ int loadBooks(std::vector<book> &books, const char* filename) {
 	book book;
 
 	inFile.open(filename);
+
 	if (!inFile.is_open()) {
 		return COULD_NOT_OPEN_FILE;
 	} else {
@@ -57,6 +58,7 @@ int loadBooks(std::vector<book> &books, const char* filename) {
 
 				ss.clear();
 			}
+			inFile.close();
 			return SUCCESS;
 		}
 	}
@@ -69,7 +71,33 @@ int loadBooks(std::vector<book> &books, const char* filename) {
  * 			SUCCESS if all data is saved
  * */
 int saveBooks(std::vector<book> &books, const char* filename) {
-	return SUCCESS;
+	ofstream outFile;
+	outFile.open(filename);
+
+	if (!outFile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	} else {
+		if (books.size() <= 0) {
+			return NO_BOOKS_IN_LIBRARY;
+		} else {
+
+			//TODO sort array by ID number
+			string data;
+
+			for (unsigned int i = 0; i < books.size() - 1; i++) {
+
+				data = std::to_string(books[i].book_id) + "," + books[i].title
+						+ "," + books[i].author + ","
+						+ std::to_string(books[i].state) + ","
+						+ std::to_string(books[i].loaned_to_patron_id);
+				outFile << data << std::endl;
+			}
+
+			outFile.close();
+			return SUCCESS;
+		}
+	}
+
 }
 
 /* clears, then loads patrons from the file filename
@@ -78,7 +106,43 @@ int saveBooks(std::vector<book> &books, const char* filename) {
  * 			SUCCESS if all data is loaded
  * */
 int loadPatrons(std::vector<patron> &patrons, const char* filename) {
-	return SUCCESS;
+	ifstream inFile;
+	string line;
+	string token;
+	stringstream ss;
+	const char CHAR_TO_SEARCH_FOR = ',';
+	patron patron;
+
+	inFile.open(filename);
+
+	if (!inFile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	} else {
+		if (inFile.peek() == std::ifstream::traits_type::eof()) {
+			return NO_PATRONS_IN_LIBRARY;
+		} else {
+
+			while (!inFile.eof()) {
+				getline(inFile, line);
+				ss.str(line);
+
+				getline(ss, token, CHAR_TO_SEARCH_FOR);
+				patron.patron_id = atoi(token.c_str());
+
+				getline(ss, patron.name, CHAR_TO_SEARCH_FOR);
+
+				getline(ss, token, CHAR_TO_SEARCH_FOR);
+				patron.number_books_checked_out = atoi(token.c_str());
+
+				patrons.push_back(patron);
+
+				ss.clear();
+			}
+			inFile.close();
+			return SUCCESS;
+		}
+	}
+
 }
 
 /* serializes patrons to the file filename
@@ -87,5 +151,29 @@ int loadPatrons(std::vector<patron> &patrons, const char* filename) {
  * 			SUCCESS if all data is saved
  * */
 int savePatrons(std::vector<patron> &patrons, const char* filename) {
-	return SUCCESS;
+	ofstream outFile;
+	outFile.open(filename);
+
+	if (!outFile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	} else {
+		if (patrons.size() <= 0) {
+			return NO_PATRONS_IN_LIBRARY;
+		} else {
+
+			//TODO sort array by ID number
+			string data;
+
+			for (unsigned int i = 0; i < patrons.size() - 1; i++) {
+
+				data = std::to_string(patrons[i].patron_id) + ","
+						+ patrons[i].name + ","
+						+ std::to_string(patrons[i].number_books_checked_out);
+				outFile << data << std::endl;
+			}
+
+			outFile.close();
+			return SUCCESS;
+		}
+	}
 }
